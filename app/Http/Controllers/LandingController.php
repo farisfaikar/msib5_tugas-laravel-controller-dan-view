a5250d8aa281f7d56000f784fb05781f21f47aa1
farisfaikar <farisfaikar.r@gmail.com>
2023-09-20 23:32:11 +0700

    typo: fixed spelling error

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -8,7 +8,7 @@ class LandingController extends Controller
 {
   function index() {
     $data['title'] = 'Faris Faikar';
-    $data['description'] = 'I am Faris Faikar, a Software Engineer based in Indonesia. Pave the ways for your projects and contact me if you have any projects in need of a competent, eficient and reliable programmer!';
+    $data['description'] = 'I am Faris Faikar, a Software Engineer based in Indonesia. Pave the ways for your projects and contact me if you have any projects in need of a competent, efficient and reliable programmer!';
 
     return view('landing', compact('data'));
   }
